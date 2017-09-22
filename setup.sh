5b622d325a2b91ec825b668dfacda41387e67504
Jason Cramer <jason.t.cramer@gmail.com>
2017-09-22 05:29:00 -0400

    Clean up setup file and add dependency

--- setup.sh
+++ setup.sh
@@ -3,8 +3,6 @@
 BIN_DIR="./bin"
 CONDA_DIR="$BIN_DIR/miniconda"
 CONDA_BIN_DIR="$CONDA_DIR/bin"
-CONDA_EXC_PATH="$CONDA_BIN_DIR/conda"
-CONDA_PIP_PATH="$CONDA_BIN_DIR/pip"
 
 # Make bin folder
 mkdir -p $BIN_DIR
@@ -13,6 +11,6 @@ mkdir -p $BIN_DIR
 wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -O $BIN_DIR/Miniconda2-latest-Linux-x86_64.sh
 bash $BIN_DIR/Miniconda2-latest-Linux-x86_64.sh -p $CONDA_DIR -b
 
-# Download ffmpeg module
+# Install conda dependencies
 $CONDA_BIN_DIR/conda install -c conda-forge ffmpeg -y
-$CONDA_BIN_DIR/pip install --upgrade youtube_dl
+$CONDA_BIN_DIR/pip install --upgrade -r ./requirements.txt
